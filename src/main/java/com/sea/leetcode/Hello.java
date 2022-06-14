4ff42d84acf6b83e3c54ecc66bba908132c83af9
wanglianhai <wanglianhai@kuaishou.com>
2022-06-14 23:35:34 +0800

    init

---
+++ src/main/java/com/sea/leetcode/Hello.java
@@ -0,0 +1,11 @@
+package com.sea.leetcode;
+
+/**
+ * @author wanglianhai <wanglianhai@kuaishou.com>
+ * Created on 2022-06-14
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
