d23829539ab026b752db84a42e3cba92bf9d7c8f
wanglianhai <wanglianhai@lixiang.com>
2022-11-09 19:08:33 +0800

    init

---
+++ src/main/java/com/sea/Main.java
@@ -0,0 +1,11 @@
+package com.sea;
+
+/**
+ * @author: wanglianhai@lixiang.com
+ * @date: ${DATE} ${TIME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
